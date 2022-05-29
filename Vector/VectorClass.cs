28e09d7ae932658634eeef8aadd078229484219e
lucjan25 <lucjan25.t@gmail.com>
2022-05-30 00:00:44 +0200

    Add project files.

---
+++ Vector/VectorClass.cs
@@ -0,0 +1,178 @@
+﻿using System;
+
+namespace Vector
+{
+    public interface IVector
+    {
+        double Abs();
+        double Cdot(IVector vector);
+        double[] GetComponents();
+    }
+
+    public class Vector2D : IVector
+    {
+        protected double x, y;
+        public Vector2D()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Vector2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double Abs()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        public double Cdot(IVector vector)
+        {
+            double[] vector2comp = vector.GetComponents();
+            return this.x * vector2comp[0] + this.y * vector2comp[1];
+        }
+
+        public double[] GetComponents()
+        {
+            return new double[] { this.x, this.y };
+        }
+    }
+    public class Polar2DInheritance : Vector2D
+    {
+        public double GetAngle()
+        {
+            double[] comps = this.GetComponents();
+            return Math.Atan(comps[1] / comps[0]);
+        }
+    }
+    public interface IPolar2D
+    {
+        double GetAngle();
+        double Abs();
+    }
+    public class Polar2DAdapter : IPolar2D, IVector
+    {
+        private readonly Vector2D srcVector;
+        public Polar2DAdapter(Vector2D srcVector)
+        {
+            this.srcVector = srcVector;
+        }
+
+        public double Abs()
+        {
+            return ((IVector)srcVector).Abs();
+        }
+
+        public double Cdot(IVector vector)
+        {
+            return ((IVector)srcVector).Cdot(vector);
+        }
+        public double[] GetComponents()
+        {
+            return ((IVector)srcVector).GetComponents();
+        }
+        public double GetAngle()
+        {
+            double[] comps = this.GetComponents();
+            return Math.Atan(comps[1] / comps[0]);
+        }
+    }
+    public class Vector3DInheritance : Vector2D
+    {
+        double z;
+
+        public Vector3DInheritance() : base()
+        {
+            z = 0;
+        }
+
+        public Vector3DInheritance(double x, double y) : base(x, y)
+        {
+            z = 0;
+        }
+
+        public Vector3DInheritance(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public new double Abs()
+        {
+            return Math.Sqrt(Math.Pow(this.x, 2) + Math.Pow(this.y, 2) + Math.Pow(this.z, 2));
+        }
+        public new double Cdot(IVector vector)
+        {
+            double[] vector3comp = vector.GetComponents();
+            return this.x * vector3comp[0] + this.y * vector3comp[1] + this.z * vector3comp[2];
+        }
+        public new double[] GetComponents()
+        {
+            return new double[] { this.x, this.y, this.z };
+        }
+        public Vector3DInheritance Cross(IVector vector)
+        {
+            double[] compsA = this.GetComponents();
+            double[] compsB = vector.GetComponents();
+            return new Vector3DInheritance(
+                compsA[1] * compsB[2] - compsB[1] * compsA[2], 
+                -(compsA[0] * compsB[2] - compsA[2] * compsB[0]), 
+                compsA[0] * compsB[1] - compsA[1] * compsB[0]
+                );
+        }
+        public IVector GetSrcV()
+        {
+            return new Vector2D(this.x, this.y);
+        }
+    }
+    public class Vector3DDecorator : IVector
+    {
+        private readonly IVector srcVector;
+        double z;
+        public Vector3DDecorator(IVector srcVector)
+        {
+            this.srcVector = srcVector;
+            this.z = 0;
+        }
+        public Vector3DDecorator(IVector srcVector, double z)
+        {
+            this.srcVector = srcVector;
+            this.z = z;
+        }
+
+        public double Abs()
+        {
+            return srcVector.Abs();
+        }
+
+        public double Cdot(IVector vector)
+        {
+            return srcVector.Cdot(vector);
+        }
+
+        public double[] GetComponents()
+        {
+            double[] comps = srcVector.GetComponents();
+            Array.Resize(ref comps, comps.Length + 1);
+            comps[2] = this.z;
+            return comps;
+        }
+        public Vector3DDecorator Cross(IVector vector)
+        {
+            double[] compsA = this.GetComponents();
+            double[] compsB = new Vector3DDecorator(vector).GetComponents();
+            double nx, ny, nz;
+            nx = compsA[1] * compsB[2] - compsB[1] * compsA[2];
+            ny = -(compsA[0] * compsB[2] - compsA[2] * compsB[0]);
+            nz = compsA[0] * compsB[1] - compsA[1] * compsB[0];
+
+            return new Vector3DDecorator(new Vector2D(nx, ny), nz);
+        }
+        public IVector GetSrcV()
+        {
+            return srcVector;
+        }
+    }
+}
---
+++ VectorProgram/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Vector;
+
+namespace VectorProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector2D v1 = new(2.5, 10.0);
+            Vector2D v2 = new(4.0, 0.3);
+            Vector2D v3 = new(5.0, 1.0);
+
+            double[] compsv1 = v1.GetComponents();
+            double[] compsv2 = v2.GetComponents();
+            double[] compsv3 = v3.GetComponents();
+
+            double anglev1 = new Polar2DAdapter(v1).GetAngle();
+            double anglev2 = new Polar2DAdapter(v2).GetAngle();
+            double anglev3 = new Polar2DAdapter(v3).GetAngle();
+
+            Console.WriteLine(string.Format("Wektor 1: Układ kartezjański: {0}, {1}; Układ biegunowy {2}, {3}", 
+                compsv1[0], compsv1[1], v1.Abs(), anglev1));
+            Console.WriteLine(string.Format("Wektor 2: Układ kartezjański: {0}, {1}; Układ biegunowy {2}, {3}", 
+                compsv2[0], compsv2[1], v2.Abs(), anglev2));
+            Console.WriteLine(string.Format("Wektor 3: Układ kartezjański: {0}, {1}; Układ biegunowy {2}, {3}", 
+                compsv3[0], compsv3[1], v3.Abs(), anglev3));
+
+            Vector3DDecorator v1_3d = new(v1);
+            Vector3DDecorator v2_3d = new(v2);
+            Vector3DDecorator v3_3d = new(v3);
+
+            double[] v1xv2 = v1_3d.Cross(v2_3d).GetComponents();
+            double[] v2xv3 = v2_3d.Cross(v3_3d).GetComponents();
+            double[] v1xv3 = v1_3d.Cross(v3_3d).GetComponents();
+
+            Console.WriteLine(string.Format("Wektor 1 i 2: Iloczyn skalarny: {0}; Iloczyn wektorowy {1}, {2}, {3}",
+                v1.Cdot(v2), v1xv2[0], v1xv2[1], v1xv2[2]));
+            Console.WriteLine(string.Format("Wektor 2 i 3: Iloczyn skalarny: {0}; Iloczyn wektorowy {1}, {2}, {3}",
+                v2.Cdot(v3), v2xv3[0], v2xv3[1], v2xv3[2]));
+            Console.WriteLine(string.Format("Wektor 1 i 3: Iloczyn skalarny: {0}; Iloczyn wektorowy {1}, {2}, {3}",
+                v1.Cdot(v3), v1xv3[0], v1xv3[1], v1xv3[2]));
+        }
+    }
+}
